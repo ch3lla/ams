fdee4c53a2d9e20c7edc27752cd5b9c2657f0bc0
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-10-14 13:28:41 +0100

    auth fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,9 +2,13 @@ import { NextFunction, Response } from "express";
 import { verify, sign } from "jsonwebtoken";
 
 export const authenticate = (req: any, res: Response, next: NextFunction) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ message: "Access denied" });
+    const { authorization } = req.headers;
 
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'No Token, please login.' });
+    }
+
+    const token = authorization.split(' ')[1];
     try {
         const decoded = verify(token, process.env.JWT_SECRET_KEY!);
         req.user = decoded;
@@ -14,14 +18,18 @@ export const authenticate = (req: any, res: Response, next: NextFunction) => {
     }
 };
 
-export const authenticateLecturer = (req: any, res: Response, next: NextFunction) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ message: "Access denied" });
+export const authenticateLecturer = (req: any, res: any, next: NextFunction) => {
+    const { authorization } = req.headers;
 
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'No Token, please login.' });
+    }
+
+    const token = authorization.split(' ')[1];
     try {
         const decoded = verify(token, process.env.JWT_SECRET_KEY!);
         req.user = decoded;
-        if (req.user.role !== "Lecturer"){
+        if (req.user.role != "lecturer"){
             res.status(400).json({ message: "Unauthorized" });
             return;
         }
@@ -29,8 +37,4 @@ export const authenticateLecturer = (req: any, res: Response, next: NextFunction
     } catch (error) {
         res.status(400).json({ message: "Invalid token" });
     }
-};
-
-export const generateToken = (id: string, role: string) => {
-    return sign({ id, role }, process.env.JWT_SECRET_KEY!, { expiresIn: "1h" });
 };
