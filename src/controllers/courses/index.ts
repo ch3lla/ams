8bd8734940809bdf88f1fb61ce0b908367ed275c
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-10-14 17:11:47 +0100

    course updates

--- src/controllers/courses/index.ts
+++ src/controllers/courses/index.ts
@@ -31,8 +31,109 @@ const addCourse = async (req: any, res: any) => {
         console.error(error);
         res.status(500).json({ message: 'Error adding course' });
     }
-}
+};
+
+const viewAllCourses = async (req: any, res: any) => {
+    const { _id } = req.user;
+    try {
+        const coursesUnderLecturer = await Course.find({ lecturer: _id })
+            .select('course_code course_name qr_code semester venue')
+            .populate({
+                path: 'venue',
+                select: 'name_of_venue',
+            });
+
+        if (!coursesUnderLecturer.length) {
+            return res.status(404).json({ message: 'No courses found for this lecturer.' });
+        }
+        res.status(200).json(coursesUnderLecturer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error adding course' });
+    }
+};
+
+const getSingleCourse = async (req: any, res: any) => {
+    const { _id } = req.user;
+    const course_id = req.params.courseId;
+    if (!course_id){
+        return res.status(400).json({mesage: "Invalid parameters"});
+    }
+    try {
+        const singleCourse = await Course.findOne({_id: course_id, lecturer: _id})
+            .select('course_code course_name qr_code semester venue')
+            .populate({
+                path: 'department',
+                select: 'name'
+            })
+            .populate({
+                path: 'venue',
+                select: 'name_of_venue',
+            });
+
+        if (!singleCourse) {
+            return res.status(404).json({ message: 'Course not found.' });
+        }
+        res.status(200).json(singleCourse);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error adding course' });
+    }
+};
+
+const updateCourse = async (req: any, res: any) => {
+    const { _id } = req.user;
+    const course_id = req.params.courseId;
+    const { ...course_data } = req.body;
+    if (!course_id){
+        return res.status(400).json({mesage: "Invalid parameters"});
+    }
+    try {
+        const filteredData = Object.fromEntries(
+            Object.entries(course_data).filter(([_, value]) => value !== null && value !== undefined)
+        );
+
+        const updatedCourse = await Course.findOneAndUpdate(
+            { _id: course_id, lecturer: _id },
+            { $set: filteredData },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedCourse) {
+            return res.status(404).json({ message: "Course not found or you do not have permission to update this course." });
+        }
+
+        res.status(200).json({ message: "Course updated successfully.", course: updatedCourse });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error adding course' });
+    }
+};
+
+const removeCourse = async (req: any, res: any) => {
+    const { _id } = req.user;
+    const course_id = req.params.courseId;
+    if (!course_id){
+        return res.status(400).json({mesage: "Invalid parameters"});
+    }
+    try {
+        const deletedCourse = await Course.findOneAndDelete({_id: course_id, lecturer: _id});
+
+        if (!deletedCourse){
+            return res.status(404).json({message: "Course not found."});
+        }
+
+        res.status(200).json({ message: "Course deleted."});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error adding course' });
+    }
+};
 
 export {
-    addCourse
+    addCourse,
+    viewAllCourses,
+    getSingleCourse,
+    updateCourse,
+    removeCourse,
 }
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import { registerStudent, loginStudent, registerLecturer, loginLecturer } from '../../controllers/auth';
 import { authenticateLecturer } from '../../middleware/auth';
-import { addCourse } from '../../controllers/courses';
+import { addCourse, getSingleCourse, removeCourse, updateCourse, viewAllCourses } from '../../controllers/courses';
 const router = express.Router();
 
+// AUTHENTICATION ROUTES
 router.post('/auth/students/register', registerStudent);
 router.post('/auth/students/login', loginStudent);
 router.post('/auth/lecturers/register', registerLecturer);
 router.post('/auth/lecturers/login', loginLecturer);
 
+// LECTURER ROUTES
 router.post('/lecturers/addCourse', authenticateLecturer, addCourse);
+router.get('/lecturers/courses', authenticateLecturer, viewAllCourses);
+router.get('/lecturers/courses/:courseId', authenticateLecturer, getSingleCourse);
+router.patch('/lecturer/courses/:courseId', authenticateLecturer, updateCourse);
+router.delete('/lecturers/courses/:courseId', authenticateLecturer, removeCourse);
 
 export default router;
